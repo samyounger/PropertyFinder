604683d5887b4d71f7158c2a6f5b6769e5e8f73d
samyounger <sam_younger@hotmail.com>
2016-11-29 20:03:31 +0000

    about half way through the tutorial

--- index.ios.js
+++ index.ios.js
@@ -2,3 +2,33 @@
 
 var React = require('react');
 var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
+
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+ReactNative.AppRegistry.registerComponent("PropertyFinder", function() {
+  return PropertyFinderApp;
+});
